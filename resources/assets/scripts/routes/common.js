6384e2ff22de786d61344c6de10bee96b8c98ec5
Alvin Manansala <aomanansala@gmail.com>
2019-04-04 21:09:14 +0800

    add mobile menu

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -33,6 +33,18 @@ export default {
 
         j.preventDefault();
     });
+
+    $('.mobile-menu-icon i').click(function() {
+        if ($(this).hasClass('fa-bars')) {
+            $(this).removeClass('fa-bars').addClass('fa-times');
+        } else if ($(this).hasClass('fa-times')) {
+            $(this).removeClass('fa-times').addClass('fa-bars');
+        }
+
+        $('#mobile-menu-container').animate({
+            height: 'toggle',
+        }, 500);
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,23 @@
 <div id="header-container">
     <div class="grid-container">
         <div class="grid-x grid-padding-x">
-            <div class="cell medium-3 large-3">
+            <div class="cell small-4 medium-3 large-3">
                 <a id="logo" href="{{ $site_url }}">
                     <img src="{{ $logo['url'] }}">
                 </a>
             </div>
 
-            <div class="cell medium-9 large-9 text-right">
+            <div class="mobile-menu-icon cell small-8 text-right show-for-small-only">
+                <i class="fal fa-bars"></i>
+            </div>
+
+            <div class="cell medium-9 large-9 text-right hide-for-small-only">
                 @php wp_nav_menu(array('menu' => 'Main Menu', 'menu_class' => 'clearfix', 'container' => false)) @endphp
             </div>
         </div>
+    </div>
+
+    <div id="mobile-menu-container" style="display: none;">
+        @php wp_nav_menu(array('menu' => 'Main Menu', 'menu_class' => 'clearfix', 'container' => false)) @endphp
     </div>
 </div>
