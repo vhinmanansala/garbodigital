d3eb219e5feb5ea6becfe0b8363f2604a5ace686
Alvin Manansala <aomanansala@gmail.com>
2019-04-22 19:46:02 +0800

    remove our team section

--- resources/views/page-about.blade.php
+++ resources/views/page-about.blade.php
@@ -18,29 +18,6 @@
                 </div>
             </div>
         </div>
-
-        <div id="our-team-section">
-            <div class="grid-container">
-                <div class="grid-x grid-padding-x justified-center">
-                    <div class="medium-6 large-6 text-center">
-                        <h3>{{ $meet_our_team_title }}</h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="grid-container">
-                <div class="grid-x grid-padding-x medium-up-4 large-up-4">
-                    @foreach ($members as $member)
-                        <div class="team-member cell">
-                            <img src="{{ $member['picture']['url'] }}" alt="{{ $member['picture']['title'] }}">
-                            <h6>{{ $member['name'] }}</h6>
-                            <span>{{ $member['position'] }}</span>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
         @include('partials.customers')
     @endwhile
 @endsection
